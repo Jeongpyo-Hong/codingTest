8203f5bfa1c7a93e4afba135f4c15ea5ec7a02b1
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2023-01-03 23:43:14 +0900

    코딩테스트 124 나라의 숫자

--- programmers/2-게임 맵 최단거리.js
+++ programmers/2-게임 맵 최단거리.js
@@ -1,23 +1,23 @@
-function solution(maps) {
-  let answer = 0;
+// function solution(maps) {
+//   let answer = 0;
 
-  for (let i = 0; i < maps.length; i++) {
-    for (let j = 0; j < maps[i].length; j++) {
-      if (maps[i][j]) {
-        answer++;
-      }
-    }
-  }
+//   for (let i = 0; i < maps.length; i++) {
+//     for (let j = 0; j < maps[i].length; j++) {
+//       if (maps[i][j]) {
+//         answer++;
+//       }
+//     }
+//   }
 
-  return answer;
-}
+//   return answer;
+// }
 
-console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
-  ])
-);
+// console.log(
+//   solution([
+//     [1, 0, 1, 1, 1],
+//     [1, 0, 1, 0, 1],
+//     [1, 0, 1, 1, 1],
+//     [1, 1, 1, 0, 1],
+//     [0, 0, 0, 0, 1],
+//   ])
+// );
